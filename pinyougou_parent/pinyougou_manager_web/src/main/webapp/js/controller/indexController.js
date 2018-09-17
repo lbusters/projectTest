1422f34a789ea505f5e8597fb1d8f45a77867d6b
zzz <466085138@qq.com>
2018-09-17 12:05:20 +0800

    dd

--- pinyougou_parent/pinyougou_manager_web/src/main/webapp/js/controller/indexController.js
+++ pinyougou_parent/pinyougou_manager_web/src/main/webapp/js/controller/indexController.js
@@ -1,4 +1,6 @@
 app.controller("indexController",function($scope,userService){
+	
+	
 	$scope.showName=function(){
 		userService.showName().success(function(response){
 			$scope.username=JSON.parse(response);
