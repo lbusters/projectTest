15494ccf1ffb8545a224ce027cf88fed9f81c65f
zzz <466085138@qq.com>
2018-09-17 12:07:08 +0800

    hh

--- pinyougou_parent/pinyougou_order_interface/src/main/java/com/pinyougou/order/service/OrderService.java
+++ pinyougou_parent/pinyougou_order_interface/src/main/java/com/pinyougou/order/service/OrderService.java
@@ -1,9 +1,20 @@
 package com.pinyougou.order.service;
 
+import java.util.List;
+
 import com.pinyougou.pojo.TbOrder;
+
+import groupEntity.ChartsView;
 
 public interface OrderService {
 
 	void save(TbOrder order);
+
+	List<ChartsView> findAllByTimeAndSelled();
+
+	ChartsView findPayGoods();
+
+	ChartsView findPayOfYear();
+
 
 }
